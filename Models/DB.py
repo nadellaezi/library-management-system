c2373cec4a7e99aa5732108852338c6fdf39fe46
nadellaezi <nadellaezisabrina@gmail.com>
2025-07-28 22:20:02 +0700

    ok

--- Models/DB.py
+++ Models/DB.py
@@ -4,10 +4,10 @@ from pymysql.cursors import DictCursor
 
 class DB(object):
 	"""Initialize mysql database """
-	host = "localhost"
+	host = "switchback.proxy.rlwy.net"
 	user = "root"
-	password = ""
-	db = "lms"
+	password = "tqecpfFAfbDNFupztCgwmMoCXedZhWzq"
+	db = "railway"
 	table = ""
 
 	def __init__(self, app):
@@ -15,6 +15,8 @@ class DB(object):
 		app.config["MYSQL_DATABASE_USER"] = self.user;
 		app.config["MYSQL_DATABASE_PASSWORD"] = self.password;
 		app.config["MYSQL_DATABASE_DB"] = self.db;
+		app.config['MYSQL_DATABASE_HOST'] = 'switchback.proxy.rlwy.net';
+		app.config['MYSQL_DATABASE_PORT'] = 46179;
 
 		self.mysql = MySQL(app, cursorclass=DictCursor)
 
